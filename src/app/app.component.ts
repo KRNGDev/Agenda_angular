ab7b5dd474513468fc06047f9ea8bcb40e6084d3
Alberto Lopez Martinez <krng.taure@gmail.com>
2024-07-24 20:43:14 +0200

    sum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabeceraComponent } from "./components/cabecera/cabecera.component"
+import { ContenedorComponent } from "./components/contenedor/contenedor.component"
+import { FooterComponent } from "./components/footer/footer.component"
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabeceraComponent],
+  imports: [RouterOutlet, FooterComponent, ContenedorComponent, CabeceraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
